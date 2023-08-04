2b399dba02a62b5c5b9ef3a0c8a9806e403e7afe
tirzasrwn <tirzasrwn@gmail.com>
2023-08-04 10:54:11 +0700

    fix: User database model.

--- internal/models/user.go
+++ internal/models/user.go
@@ -9,8 +9,6 @@ import (
 
 type User struct {
 	ID        int       `json:"id"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
 	Email     string    `json:"email"`
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"-"`

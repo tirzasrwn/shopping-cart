c38aaf08d7115fc161be971cc7768cae75ea536a
tirzasrwn <tirzasrwn@gmail.com>
2023-08-04 20:40:28 +0700

    fix: Add on delete cascade. Cart will be delete when user deleted.

--- internal/migration/000004_create_cart_table.up.sql
+++ internal/migration/000004_create_cart_table.up.sql
@@ -3,6 +3,6 @@ create table "cart" (
   user_id int,
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES "user"(id) 
+  CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES "user"(id) ON DELETE CASCADE 
 );
 
